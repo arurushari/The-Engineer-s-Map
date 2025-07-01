2df03de82c0e56b2da1a6553cf69db296cd342f6
arurushari <aruzhan0410kz@gmail.com>
2025-07-01 17:01:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuToggle = document.getElementById('menu-toggle');
+  const menuList = document.getElementById('menu-list');
+  const overlay = document.getElementById('overlay');
+
+  function toggleMenu() {
+    menuList.classList.toggle('active');
+    overlay.classList.toggle('active');
+  }
+
+  menuToggle.addEventListener('click', toggleMenu);
+
+  // Закрываем меню при клике на оверлей
+  overlay.addEventListener('click', () => {
+    menuList.classList.remove('active');
+    overlay.classList.remove('active');
+  });
+
+  // Закрываем меню при клике на пункт меню
+  menuList.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      menuList.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  });
